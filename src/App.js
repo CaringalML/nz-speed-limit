6eaf3656d6cad89d466a6110e3c505d40462504a
caringallawrence <caringallawrence5@gmail.com>
2024-10-05 16:25:30 +1300

    initsh

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 // src/App.js
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import SpeedLimit from './components/SpeedLimit';
+import SpeedLimitList from './components/SpeedLimitList'; // Ensure this component is correctly imported
 
 function App() {
   return (
-    <div className="App">
-      <SpeedLimit />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* Define your routes here */}
+          <Route path="/nz-speed-limit" element={<SpeedLimit />} />
+          <Route path="/speed-limit-list" element={<SpeedLimitList />} />
+          
+          {/* Optional: Define a default route (commented out) */}
+          {/* <Route path="/" element={<Navigate to="/nz-speed-limit" />} /> */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
